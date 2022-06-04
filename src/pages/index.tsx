205ba3d629f3d0073595436f01815044299f1a1f
George Lucas Aplicativos <44735373+georgelucasdev@users.noreply.github.com>
2022-06-04 16:50:25 -0300

    Main-Site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import styles from '../styles/home.module.scss';
-
 import Image from 'next/image';
+
 
 
 export default function Home() {
@@ -10,9 +10,21 @@ export default function Home() {
       <Head>
         <title>George Lucas Aplicativos </title>
       </Head>
+      <hr className={styles.hrtop}/>
       <div className={styles.containerHeader}>
         <h1></h1>
+        <img
+        src="/foto1.png"
+         />
+        <p>
+          Em constante evolução em desenvolvimento web para front end;
+          focado em criar novas experiências para o usuário com as tecnologias e frameworks mais utilizadas do mercado
+          : ReactJS, NextJS, Typescript, NodeJS em combinação com banco de dados em servidor. De caráter honesto,
+          busca o estímulo, organização, compartilhamento de conhecimento e o contínuo desenvolvimento pessoal e
+          profissional do grupo de trabalho.
+        </p>
       </div>
+      <hr className={styles.hr}/>
       <section className={styles.ctaText}>
       </section>
     </>
